757ee91536c1214635f28243f12920d9f32afaa2
Diegom14 <diego.munozv@mail.udp.cl>
2016-11-26 18:56:45 -0300

    link de la banda encontrado

--- tarea3.py
+++ tarea3.py
@@ -11,20 +11,31 @@ import re # expresiones regulares
 
 if __name__ == "__main__":
     # descargar una url
-    url = 'http://musicbrainz.org/artist/e99f6d62-f62b-4e1e-8593-33d5696d85f0'
+    url = 'http://musicbrainz.org/search?query=metallica&type=artist&method=indexed'
     response = urlopen(url)
     html = response.read()
     html = html.decode('utf-8')
+
+    
+    #lleva al link con mayor score
+    link = re.findall(r'<a href="/artist/([^<]+)" title=', html)
+    url = "http://musicbrainz.org/artist/"+link[0]
+	response = urlopen(url)
+    html = response.read()
+    html = html.decode('utf-8')
+
+    #busca el la fecha de inicio, pais de origen y youtube de la banda
 
     youtube = re.findall(r'<a href="//www.youtube([^<]+)">[^<]+</a>', html)
     fundacion = re.findall(r'<dt>Founded:</dt>\n<dd>([^<]+)', html)
 
     paisOrigen = re.findall(r'<dd class="area"><span class="+[^<]+"><a href="+[^<]+"><bdi>([^<]+)</bdi></a></span></dd>', html)
     
+
+    #imprime la información
     for i in range(len(youtube)):
     	print("Link", i+1,":" ,"www.youtube"+youtube[i])
 
-    print("Fecha de fundación: ",fundacion)
+    print("Fecha de fundación: ",fundacion[0])
 
-    print(paisOrigen)
-    
+    print("Pais de origen :",paisOrigen[0])
